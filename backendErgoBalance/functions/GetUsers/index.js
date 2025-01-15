909c4a734ae95623c58278b1ded73a49327802ba
Ida Walther <idahwalther@icloud.com>
2025-01-15 18:19:02 +0100

    It's now possible to get all users information, however, that also includes the password. This needs to be fixed

--- backendErgoBalance/functions/GetUsers/index.js
+++ backendErgoBalance/functions/GetUsers/index.js
@@ -1,8 +1,15 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
-};
+const {db} = require('../../services/index.js');
+const {sendResponse, sendError } = require('../../responses/index');
+
+const getUsers = async (event) => {
+  try {
+    const data = await db.scan({
+      TableName: 'ergousers-db'
+    })
+    return sendResponse(200, {success: true, data: data.Items})
+  } catch(error) {
+    return sendError(500, {message: error.message})
+  }
+}
+
+exports.handler = getUsers
