4b1eb0b69e4bd335fc4a41ba645a52127af9d845
Ida Walther <idahwalther@icloud.com>
2025-02-21 14:54:34 +0100

    If you click on an exercise you get more information about it

--- frontendErgoBalance/src/views/setupExercisesView/SetupExercisesView.vue
+++ frontendErgoBalance/src/views/setupExercisesView/SetupExercisesView.vue
@@ -9,8 +9,12 @@ import { onMounted, ref } from 'vue'
 
 const token = localStorage.getItem('token')
 const username = ref('')
+const exname = ref('')
+const exdesc = ref('')
+const eximage = ref('')
 const showExercises = ref(false)
 const showMyProgram = ref(false)
+const showInfo = ref(false)
 const program = ref()
 const allExercises = ref()
 
@@ -108,20 +112,38 @@ const addExercise = async (pk: string, sk:string, name:string, desc: string, ima
     throw error
   }
 }
+
+const showMoreInfo = (name : string, desc : string, image : string) => {
+  console.log('show Info', name, desc, image)
+  showInfo.value = true;
+  exname.value = name
+  exdesc.value = desc
+  eximage.value = image
+
+}
+
+const dontShow = () => {
+  showInfo.value = false
+}
 </script>
 
 <template>
   <section class="setupExercisesView__wrapper">
+    <router-link to="/main">
+      <figure>
+        <img class="setupExercisesView__logo" src="../../assets/images/ergoBalanceLogo.png" alt="logo som tar dig tillbaka till startsidan" />
+      </figure>
+      </router-link>
     <section class="setupExercisesView__header">
       <Button @click="myProgram" class="setupExercisesView__button">Mitt program</Button>
       <Button @click="showAllExercises"  class="setupExercisesView__button">Alla övningar</Button>
     </section>
     <section v-if="showMyProgram">
-      <h2 class="setupExercisesView__title">Mitt {{ program.name }}</h2>
+      <h2 class="setupExercisesView__title">Ta bort övningar</h2>
       <section class="setupExercisesView__container">
       <ul v-for:="item in program.exercises" class="setupExercisesView__ul">
         <li class="setupExercisesView__li">{{ item.name }}
-        <Button @click="removeExercise(item.pk, item.sk)"  class="setupExercisesView__button setupExercisesView__button--remove">X</Button>
+        <Button @click="removeExercise(item.pk, item.sk)"  class="setupExercisesView__button setupExercisesView__button--remove">-</Button>
         </li>
       </ul>
       </section>
@@ -130,11 +152,24 @@ const addExercise = async (pk: string, sk:string, name:string, desc: string, ima
       <h2 class="setupExercisesView__title">Lägg till övningar</h2>
       <section class="setupExercisesView__container">
         <ul v-for:="item in allExercises" class="setupExercisesView__ul">
-          <li class="setupExercisesView__li">{{ item.name }}
-          <Button @click="addExercise(item.pk, item.sk, item.name, item.desc, item.image)"  class="setupExercisesView__button setupExercisesView__button--add">+</Button>
+          <li>
+            <ul class="setupExercisesView__ul setupExercisesView__ul--inner">
+              <li @click="showMoreInfo(item.name, item.desc, item.image)">
+                {{ item.name }}
+              </li>
+              <li>
+                <Button @click="addExercise(item.pk, item.sk, item.name, item.desc, item.image)"  class="setupExercisesView__button setupExercisesView__button--add">+</Button>
+              </li>
+            </ul>
           </li>
         </ul>
       </section>
     </section>
+    <div v-if="showInfo" class="setupExercisesView__modal">
+      <Button class="setupExercisesView__modal-button" @click="dontShow">Tillbaka</Button>
+      <h1 class="setupExercisesView__modal-title">{{exname}}</h1>
+      <p class="setupExercisesView__modal-text">{{ exdesc }}</p>
+      <img :src="eximage" class="setupExercisesView__modal-image">
+    </div>
   </section>
 </template>
