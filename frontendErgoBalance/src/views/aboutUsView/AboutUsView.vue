b9df20faa49244b797e5f06fd3c91ef783c57d63
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2025-03-03 14:23:56 +0100

    added padding and text

--- frontendErgoBalance/src/views/aboutUsView/AboutUsView.vue
+++ frontendErgoBalance/src/views/aboutUsView/AboutUsView.vue
@@ -12,8 +12,8 @@ import { RouterLink } from 'vue-router';
       </RouterLink>
       <h1 class="aboutUsView__header">Om oss</h1>
       <section class="aboutUsView__section">
-      <h3 class="aboutUsView__section-header">Två nördar och en briljant idé</h3>
-      <p> Hej! Vi är Lisa och Ida, två klasskamrater som insåg att vi (och alla andra) sitter alldeles för mycket stilla på jobbet/när vi pluggar. Så vi tänkte: Vad händer om vi bygger en app som säger åt folk att röra på sig – och gör det på ett kul sätt?
+      <h3 class="aboutUsView__section-header">Två träningsglada utvecklare och en idé</h3>
+      <p> Hej! Vi är Ida och Lisa, två klasskamrater som insåg att vi (och nästan alla andra) sitter alldeles för mycket stilla på jobbet/när vi pluggar. Så vi tänkte: Vad händer om vi bygger en app som säger åt folk att röra på sig – och gör det på ett kul sätt?
         <br>
         Sagt och gjort! Som en del av vårt examensarbete på vår YH-utbildning skapade vi en ergonomiapp som inte låter dig förvandlas till en stel kontorsmumie.
       </p>
@@ -21,7 +21,7 @@ import { RouterLink } from 'vue-router';
       <p class="aboutUsView__section-imgInfo">Ida och Lisa - Skapare av ErgoBalance</p>
     </section>
     <section class="aboutUsView__section aboutUsView__section-backgroundcolor">
-      <h3 class="aboutUsView__section-header">Vad gör vår superduper-app?</h3>
+      <h3 class="aboutUsView__section-header">Vad gör appen?</h3>
       <p>
 📅 Timer som räknar ner till din nästa paus – För att du ska slippa glömma att röra på dig.
 <br>
@@ -32,7 +32,7 @@ import { RouterLink } from 'vue-router';
 ⏳ Tydlig info om hur lång pausen är – Så du vet exakt hur länge du kan pausa innan du återgår till jobbet.
 <br>
 <br>
-⚙️ Möjlighet att anpassa övningarna efter dina preferenser – Så du kan välja det som passar just dig!
+⚙️ Möjlighet att anpassa övningarna efter dina preferenser – Så du kan välja de övningar som passar bäst för dig och dina behov.
 <br>
 <br>
 Vi skapade den här appen för att vi själva behövde den – och vi hoppas att den hjälper dig också!
