b9ecb42de6ad9cca4b89af0887f25657b3df421f
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2025-03-06 11:36:17 +0100

    progressbar working

--- frontendErgoBalance/src/stores/intervalStore.ts
+++ frontendErgoBalance/src/stores/intervalStore.ts
@@ -8,12 +8,14 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
   const workInterval: Ref<number> = ref(25 * 60)
   const breakDuration: Ref<number> = ref(5 * 60)
   const overallTime: Ref<number> = ref(240 * 60)
+  const overallStartTime: Ref<number | null> = ref(null);
 
   //statusvariabler
   const isRunning: Ref<boolean> = ref(false)
   const isPaused: Ref<boolean> = ref(false)
   const currentPhase: Ref<'work' | 'break'> = ref('work')
   const remainingTime: Ref<number> = ref(0)
+  const progressPercentage: Ref<number> = ref(0)
 
   let startTime: number = 0
   let phaseStart: number = 0
@@ -24,6 +26,9 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
 
 
   function startOverallTimer(): void {
+    overallStartTime.value = Date.now()
+    requestAnimationFrame(updateProgress)
+
     overallIntervalId = setInterval(() => {
       if (!isRunning.value) return
 
@@ -71,9 +76,11 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
     isRunning.value = true
     isPaused.value = false
     startTime = Date.now()
+    overallStartTime.value = Date.now();
     currentPhase.value = 'work'
     startOverallTimer()
     runCycle()
+
   }
 
   function stop() {
@@ -84,6 +91,7 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
     if (countdownIntervalId) clearInterval(countdownIntervalId)
     if (overallIntervalId) clearInterval(overallIntervalId)
     startTime = 0
+    overallStartTime.value = null;
   }
 
   function pauseToggle() {
@@ -99,6 +107,15 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
       startTimeout()
     }
   }
+  function updateProgress() {
+    if (!isRunning.value) return
+
+    const elapsedTime = Math.floor((Date.now() - overallStartTime.value!) / 1000);
+    const percentage = (elapsedTime / overallTime.value) * 100;
+    progressPercentage.value = Math.max(0, Math.min(100, Math.floor(percentage)))
+
+    requestAnimationFrame(updateProgress);
+  };
 
   function updateSettings(newWorkInterval: number, newBreakDuration: number, newOverallTime: number): void {
     workInterval.value = newWorkInterval
@@ -114,6 +131,8 @@ export const useIntervalTimer = defineStore('intervalTimer', () => {
     isPaused,
     currentPhase,
     remainingTime,
+    overallStartTime,
+    progressPercentage,
     start,
     stop,
     pauseToggle,
