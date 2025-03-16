d0f1d1a0ef28baed0997d579b7e9f8405f7d2ecb
Ida Walther <idahwalther@icloud.com>
2025-03-16 22:35:08 +0100

    A first layout of the mainview page

--- frontendErgoBalance/src/views/mainView/MainView.vue
+++ frontendErgoBalance/src/views/mainView/MainView.vue
@@ -33,6 +33,12 @@ onMounted(() => {
     <Header />
   <section class="mainView__container">
     <h1 class="mainView__heading">Välkommen {{ username }}</h1>
+    <article class="mainView__article">
+      <img src="../../assets/images/move.webp" class="mainView__image">
+      <p><img src="../../assets/images/hourGlass.png" class="mainView__icon"> Starta ett rörelseprogram. Om inga ändring görs startas ett förinställt program som är 4 timmar långt, med 25 minuters intensivt arbete för att sedan ta en 5 minuter paus för att få in lite rörelse.</p>
+      <p><img src="../../assets/images/Timesheet.png" class="mainView__icon"> Vill du själv göra ändring i timern</p>
+      <p><img src="../../assets/images/Squats.png" class="mainView__icon mainView__icon--large"> Om du vill göra ändring i ditt program</p>
+    </article>
   </section>
   <Menu />
 </section>
