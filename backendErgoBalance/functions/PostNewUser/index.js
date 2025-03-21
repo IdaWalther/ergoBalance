6d6353ab408c1ce290f66330565427efb4d7ab15
Ida Walther <idahwalther@icloud.com>
2025-03-21 11:26:46 +0100

    Changed the starting exercises

--- backendErgoBalance/functions/PostNewUser/index.js
+++ backendErgoBalance/functions/PostNewUser/index.js
@@ -27,42 +27,42 @@ const postNewUser = async (event) => {
             "pk": "active",
             "name": "Hoppa på ett ben",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/Hoppa+p%C3%A5+ett+ben.png",
-            "desc": "Stå på ett ben med lätt böjd knäled och armarna som balanshjälp. Tryck ifrån explosivt och hoppa rakt upp, landa mjukt på samma ben och upprepa rörelsen."
+            "desc": "Stå på ett ben med lätt böjd knäled. Tryck ifrån och hoppa rakt upp, landa mjukt på samma ben och upprepa rörelsen.\nFörslag på antal: 10 repetitioner per ben X 2 sets"
         },
         {
             "sk": "09f8022e-b4e9-4f84-8f81-5e898cade23f",
             "pk": "active",
-            "name": "Dubbelhaka",
+            "name": "Hakindrag",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/_Dubbelhaka_.png",
-            "desc": "Skapa en dubbelhaka och håll i 10 sekunder. Slappna sedan av."
+            "desc": "Skapa en \"dubbelhaka\" genom att dra hakan bakåt och aningen nedåt. Håll i ca 5 sekunder. Slappna sedan av.\nFörslag på antal: 10 repetitioner X 2 sets. \n"
         },
         {
             "sk": "0bdb59b6-eeb1-4ce0-b0eb-2b7cba6d445c",
             "pk": "active",
-            "name": "Tåhävningar",
+            "name": "Tåhävningar med stöd",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/T%C3%A5h%C3%A4vningar.png",
-            "desc": "Stå med fötterna axelbrett isär och lyft långsamt hälarna från marken så att du står på tårna, håll positionen i några sekunder och sänk sedan långsamt tillbaka till startpositionen. För en extra utmaning kan du göra övningen på ett trappsteg eller en upphöjd yta."
+            "desc": "Lyft långsamt hälarna från marken så att du står på tårna, håll positionen i några sekunder och sänk sedan långsamt tillbaka till startpositionen. För extra utmaning kan du göra övningen på ett trappsteg eller en upphöjd yta.\nFörslag på antal: 20 repetitioner X 3 sets."
         },
         {
             "sk": "1de271c7-95bd-4d9b-b0e3-db936aee36b5",
             "pk": "active",
             "name": "Skulderstabilitet med gummiband",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/Skulderstabilitet+med+gummiband.png",
-            "desc": "Håll ett gummiband med båda händerna och sträck ut det ovanför huvudet, med fötterna axelbrett isär och armarna rakt upp. Dra ut bandet genom att separera händerna åt sidorna, håll spänningen i några sekunder, och återgå långsamt till startpositionen. Denna övning stärker axelmusklerna och förbättrar stabiliteten för rotatorcuffen."
+            "desc": "Håll ett gummiband sträckt ovanför huvudet. Dra isär bandet med båda armarna, håll spänningen i några sekunder innan du långsamt återgår till startpositionen. \nFörslag på antal: 10 repetitioner X 3 sets."
         },
         {
             "sk": "25c4f942-c9c4-47c2-ad83-7937d3646023",
             "pk": "active",
             "name": "Pil och båge med gummiband",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/Pil+och+b%C3%A5ge+med+gummiband.png",
-            "desc": "Pil och båge med gummiband görs genom att stå med fötterna axelbrett isär och hålla gummibandet med båda händerna. Dra ena armen bakåt som om du sträcker en pil, medan den andra armen hålls rakt framför dig. Återgå långsamt till startpositionen och upprepa, byt sedan sida."
+            "desc": "Håll gummibandet med raka armar framför dig. Dra ena armen går bakåt som om du sträcker ut en pilbåge medan den andra armen hålls kvar stadigt framför dig. Återgå långsamt till startpositionen och upprepa, byt sedan sida.\nFörslag på antal: 10 repetitioner per arm X 2 sets."
         },
         {
             "sk": "2dca55b7-bbcf-42e3-ae4d-3bcd8ad5c097",
             "pk": "active",
             "name": "Sittande axelrullningar",
             "image": "https://ergobalanceexercises.s3.eu-north-1.amazonaws.com/Sittande+axelrullningar.png",
-            "desc": "Lyft axlarna upp mot öronen, rulla dem bakåt och neråt i en cirkulär rörelse, och upprepa sedan rörelsen framåt. Fortsätt att rulla axlarna i båda riktningarna för att lindra spänningar och förbättra rörligheten i axelleden."
+            "desc": "Lyft axlarna upp mot öronen, rulla dem bakåt och neråt i en cirkulär rörelse. Upprepa sedan rörelsen framåt. Fortsätt att rulla axlarna i båda riktningarna.\nFörslag på antal: 20 repetitioner X 3 sets."
         }
     ]
 
